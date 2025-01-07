a36937d5536ab3ba90d2cb11770f6c6d9ecbd8da
Silence2361 <161140816+Silence2361@users.noreply.github.com>
2025-01-07 21:25:32 +0300

    docs: added Tag

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Users')
     .addTag('Transactions')
+    .addTag('Authorization')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
